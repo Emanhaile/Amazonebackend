0b2cd980930eefe70d7cfd1705bc9519a27cd3be
Emanhaile <emanhaile06@gmail.com>
2024-11-20 16:06:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express= require('express');
+const cors= require('cors');
+const dotenv= require('dotenv');
+dotenv.config();
+const stripe=require('stripe')
+(process.env.STRIP_KEY)
+const app =express();
+app.use(cors({orgin:true}))
+app.use(express.json());
+app.get('/', (req, res)=>{
+res.status(200).json({
+    message:"sucessfully recived"
+})
+
+})
+app.post("/api/payment/create",async(req, res)=>{
+    const total= req.query.total;
+if(total>0){
+    const payment= await stripe.paymentIntents.create({
+        amount:total,
+        currency:"usd"
+    });
+    console.log(payment)
+    res.status(200).json({
+        clientSecret:payment.client_secret,
+    });
+}
+else{
+    res.status(404).json("paymments not working");
+}
+})
+
+app.listen(4400,(err)=>{
+if(err) throw err
+console.log("the servr is running at port https://localhost:4400")
+});
